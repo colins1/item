2d6b55a2659791d306744aa48e050238a82f8887
colins1 <88490207+colins1@users.noreply.github.com>
2022-10-16 17:30:02 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,6 @@ import Content from './ContentBoard';
 import Task from './TaskColumns';
 import Engine from './Engine';
 
-new ContentBoard();
-new TaskColumns();
+new Content();
+new Task();
 new Engine();
